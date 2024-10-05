774c2d299f37343194ad72a1a6083c9cfad6048c
da578 <dzulkiflianwar2@gmail.com>
2024-10-05 19:43:31 +0700

    feat: add multiple routes in root scope
    - Route `/` for home page.
    - Route `/prestasi` for student achievement page.
    - Route `/login` for login page.
    - Route `/proses-login` for login authentication.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\RootController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("/")->group(function () {
+    Route::get("", [RootController::class, "Beranda"])->name("beranda");
+    Route::get("prestasi", [RootController::class, "Prestasi"])->name("prestasi");
+    Route::get("login", [RootController::class, "Login"])->name("login");
+    Route::post("proses-login")->middleware(AuthMiddleware::class)->name("proses-login");
 });
