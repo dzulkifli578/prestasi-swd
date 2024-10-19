3b8e67f6c8f1c2c54fb6ac9fdfb641f04a4a0f60
da578 <dzulkiflianwar2@gmail.com>
2024-10-19 10:29:41 +0700

    feat: add footer with logo, school information, and social media links

---
+++ resources/views/admin/components/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer footer-center bg-primary text-primary-content p-10">
+    <aside>
+        <img src="{{ asset('img/swadhipa.png') }}" alt="Logo Swadhipa" class="w-auto h-16">
+        <p class="text-base md:text-lg font-bold">
+            SMK Swadhipa 2 Natar
+            <br />
+            Teknologi Rekayasa dan Teknologi Informasi Komunikasi.
+        </p>
+        <p class="text-sm md:text-base font-medium">Copyright © {{ date('Y') }} - All right reserved</p>
+    </aside>
+    <nav>
+        <div class="grid grid-flow-col gap-4">
+            <a>
+                <span class="fa-brands fa-youtube text-xl cursor-pointer"></span>
+            </a>
+            <a>
+                <span class="fa-brands fa-tiktok text-xl cursor-pointer"></span>
+            </a>
+            <a>
+                <span class="fa-brands fa-instagram text-xl cursor-pointer"></span>
+            </a>
+            <a>
+                <span class="fa-brands fa-facebook text-xl cursor-pointer"></span>
+            </a>
+        </div>
+    </nav>
+</footer>
