5c091c87eda946f77d072bef680baa48e579e8dc
da578 <dzulkiflianwar2@gmail.com>
2024-10-19 08:06:37 +0700

    feat: add `Juara` model

---
+++ app/Models/Juara.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Juara
+ *
+ * Represents a champion in the competition system.
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $nama
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
+class Juara extends Model
+{
+    protected $table = "juara";
+
+    protected $primaryKey = "id";
+    
+    protected $fillable = [
+        "nama"
+    ];
+
+    public $timestamps = true;
+}
