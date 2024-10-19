4006affeb80bc61c89d72f940fb6e982d54aa35d
da578 <dzulkiflianwar2@gmail.com>
2024-10-19 08:05:36 +0700

    refactor: improve `Akun` model with additional properties and documentation

--- app/Models/Akun.php
+++ app/Models/Akun.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Akun
+ *
+ * Represents a user account in the system.
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $nama_pengguna
+ * @property string $kata_sandi
+ * @property string $email
+ * @property string|null $foto
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class Akun extends Model
 {
     protected $table = "akun";
+
+    protected $primaryKey = "id";
+
+    protected $fillable = [
+        "nama_pengguna",
+        "kata_sandi",
+        "email",
+        "foto"
+    ];
+
+    public $timestamps = true;
 }
