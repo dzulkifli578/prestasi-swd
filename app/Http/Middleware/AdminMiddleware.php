1ddcd69e58f21b19d9c8662b3db2ef41588ff067
da578 <dzulkiflianwar2@gmail.com>
2024-10-19 08:01:12 +0700

    docs: add docstrings for class and function

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -7,12 +7,21 @@ use Illuminate\Http\Request;
 use Session;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class AdminMiddleware
+ *
+ * Middleware for checking if the user has admin access.
+ *
+ * @package App\Http\Middleware
+ */
 class AdminMiddleware
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
