d20dde1ff202f87c46693fb1e75c1cbbcfe01b99
da578 <dzulkiflianwar2@gmail.com>
2024-10-19 08:06:29 +0700

    feat: add `BidangLomba` model

---
+++ app/Models/BidangLomba.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BidangLomba
+ *
+ * Represents a competition field in the system.
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $nama
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
+class BidangLomba extends Model
+{
+    protected $table = "bidang_lomba";
+
+    protected $primaryKey = "id";
+
+    protected $fillable = [
+        "nama"
+    ];
+
+    public $timestamps = true;
+}
