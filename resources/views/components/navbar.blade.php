dd322160300a51ad3f5d2621e1c9793e98eaff4d
da578 <dzulkiflianwar2@gmail.com>
2024-10-22 10:07:37 +0700

    feat: add dynamic greeting based on current time and `tentang` route

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,24 @@
 @php
+    use Carbon\Carbon;
+
+    $currentHour = Carbon::now()->format('H');
+    if ($currentHour >= 5 && $currentHour < 12) {
+        $greeting = 'Selamat Pagi';
+    } elseif ($currentHour >= 12 && $currentHour < 15) {
+        $greeting = 'Selamat Siang';
+    } elseif ($currentHour >= 15 && $currentHour < 18) {
+        $greeting = 'Selamat Sore';
+    } else {
+        $greeting = 'Selamat Malam';
+    }
+@endphp
+
+@php
     $menu = [
-        ['route' => 'beranda', 'label' => 'Beranda'],
-        ['route' => 'prestasi', 'label' => 'Prestasi'],
-        ['route' => 'login', 'label' => 'Login'],
+        ['route' => 'beranda', 'label' => 'Beranda', 'icon' => 'fa-solid fa-house'],
+        ['route' => 'prestasi', 'label' => 'Prestasi', 'icon' => 'fa-solid fa-list'],
+        ['route' => 'tentang', 'label' => 'Tentang', 'icon' => 'fa-solid fa-info'],
+        ['route' => 'login', 'label' => 'Login', 'icon' => 'fa-solid fa-right-to-bracket'],
     ];
 @endphp
 
@@ -17,20 +33,20 @@
     </div>
 </div>
 
-<!-- Drawer -->
 <div class="drawer z-10">
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-side">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
         <ul class="menu bg-base-300 text-base-content min-h-full w-52 p-6">
             <div class="w-full flex flex-col justify-center">
-                <p class="text-lg md:text-xl lg:text-2xl font-semibold">Halo</p>
+                <p class="text-lg md:text-xl lg:text-2xl font-semibold">{{ $greeting }}</p>
                 <div class="border border-primary my-4"></div>
             </div>
-            <div class="text-lg md:text-xl lg:text-2xl">
-                @foreach ($menu as $item)
-                    <li><a href="{{ route($item['route']) }}"
-                            class="{{ Route::is($item['route']) ? 'text-primary' : '' }}">{{ $item['label'] }}</a>
+            <div class="text-lg">
+                @foreach ($menu as $menu)
+                    <li><a href="{{ route($menu['route']) }}"
+                            class="{{ Route::is($menu['route']) ? 'text-primary' : '' }}"><i
+                                class="{{ $menu['icon'] }}"></i> {{ $menu['label'] }}</a>
                     </li>
                 @endforeach
             </div>
