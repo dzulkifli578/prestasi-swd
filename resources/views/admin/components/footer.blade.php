7d795fc33313103da03a1fc25275cd101eb6f5e7
da578 <dzulkiflianwar2@gmail.com>
2024-10-22 09:41:27 +0700

    refactor: improve footer layout and add social media links

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -1,27 +1,20 @@
-<footer class="footer footer-center bg-primary text-primary-content p-10">
-    <aside>
+<footer class="footer bg-primary text-primary-content items-center p-4">
+    <aside class="grid-flow-col items-center">
         <img src="{{ asset('img/swadhipa.png') }}" alt="Logo Swadhipa" class="w-auto h-16">
-        <p class="text-base md:text-lg font-bold">
-            SMK Swadhipa 2 Natar
-            <br />
-            Teknologi Rekayasa dan Teknologi Informasi Komunikasi.
-        </p>
-        <p class="text-sm md:text-base font-medium">Copyright © {{ date('Y') }} - All right reserved</p>
+        <p class="text-lg font-medium">Copyright © {{ date('Y') }} - All right reserved</p>
     </aside>
-    <nav>
-        <div class="grid grid-flow-col gap-4">
-            <a>
-                <span class="fa-brands fa-youtube text-xl cursor-pointer"></span>
-            </a>
-            <a>
-                <span class="fa-brands fa-tiktok text-xl cursor-pointer"></span>
-            </a>
-            <a>
-                <span class="fa-brands fa-instagram text-xl cursor-pointer"></span>
-            </a>
-            <a>
-                <span class="fa-brands fa-facebook text-xl cursor-pointer"></span>
-            </a>
-        </div>
+    <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a href="https://www.youtube.com/@smkswadhipa2natar">
+            <span class="fa-brands fa-youtube text-xl cursor-pointer"></span>
+        </a>
+        <a href="https://www.tiktok.com/@smkswadhipa2natar">
+            <span class="fa-brands fa-tiktok text-xl cursor-pointer"></span>
+        </a>
+        <a href="https://www.instagram.com/smkswadhipa2/">
+            <span class="fa-brands fa-instagram text-xl cursor-pointer"></span>
+        </a>
+        <a href="https://www.facebook.com/smkswadhipa2/?locale=id_ID">
+            <span class="fa-brands fa-facebook text-xl cursor-pointer"></span>
+        </a>
     </nav>
 </footer>
