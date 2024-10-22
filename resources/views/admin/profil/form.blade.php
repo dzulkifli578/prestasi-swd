07235ba07e9a18281f59882037d28c88bb85f6f5
da578 <dzulkiflianwar2@gmail.com>
2024-10-22 10:03:29 +0700

    style: add width styling to input fields for consistency

--- resources/views/admin/profil/form.blade.php
+++ resources/views/admin/profil/form.blade.php
@@ -4,15 +4,17 @@
     @method('PUT')
     <label class="input input-bordered input-primary flex items-center gap-2">
         <span class="fa-solid fa-user text-base-content"></span>
-        <input type="text" name="nama_pengguna" value="{{ $akun->nama_pengguna }}" class="grow" placeholder="Nama Pengguna" />
+        <input type="text" name="nama_pengguna" value="{{ $akun->nama_pengguna }}" class="grow w-full"
+            placeholder="Nama Pengguna" />
     </label>
     <label class="input input-bordered input-primary flex items-center gap-2">
         <span class="fa-solid fa-lock text-base-content"></span>
-        <input type="password" name="kata_sandi" class="grow" placeholder="Kata Sandi" />
+        <input type="password" name="kata_sandi" class="grow w-full" placeholder="Kata Sandi" />
     </label>
     <label class="input input-bordered input-primary flex items-center gap-2">
         <span class="fa-solid fa-envelope"></span>
-        <input type="email" name="email" value="{{ $akun->email }}" class="grow" placeholder="Email" />
+        <input type="email" name="email" value="{{ $akun->email }}" class="grow w-full"
+            placeholder="Email" />
     </label>
     <input type="file" name="foto" class="file-input file-input-bordered file-input-primary w-full" />
     <button type="submit" class="btn btn-primary w-full">Perbarui</button>
