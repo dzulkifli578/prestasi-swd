1d94a1852b4d37f9797db5cbe1ad33560aa62fdb
da578 <dzulkiflianwar2@gmail.com>
2024-10-22 09:45:31 +0700

    refactor: improve footer layout

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="footer footer-center bg-primary text-primary-content p-10">
+<footer class="footer bg-primary text-primary-content p-10">
     <aside>
         <img src="{{ asset('img/swadhipa.png') }}" alt="Logo Swadhipa" class="w-auto h-16">
-        <p class="text-base md:text-lg font-bold">
+        <p class="text-base md:text-lg font-bold my-2">
             SMK Swadhipa 2 Natar
             <br />
             Teknologi Rekayasa dan Teknologi Informasi Komunikasi.
@@ -9,17 +9,18 @@
         <p class="text-sm md:text-base font-medium">Copyright © {{ date('Y') }} - All right reserved</p>
     </aside>
     <nav>
+        <h6 class="footer-title">Sosial Media</h6>
         <div class="grid grid-flow-col gap-4">
-            <a>
+            <a href="https://www.youtube.com/@smkswadhipa2natar">
                 <span class="fa-brands fa-youtube text-xl cursor-pointer"></span>
             </a>
-            <a>
+            <a href="https://www.tiktok.com/@smkswadhipa2natar">
                 <span class="fa-brands fa-tiktok text-xl cursor-pointer"></span>
             </a>
-            <a>
+            <a href="https://www.instagram.com/smkswadhipa2/">
                 <span class="fa-brands fa-instagram text-xl cursor-pointer"></span>
             </a>
-            <a>
+            <a href="https://www.facebook.com/smkswadhipa2/?locale=id_ID">
                 <span class="fa-brands fa-facebook text-xl cursor-pointer"></span>
             </a>
         </div>
