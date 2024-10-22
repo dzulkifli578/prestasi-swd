cd61bc54e6aa5c2704530a13b03c5111f70a42ba
da578 <dzulkiflianwar2@gmail.com>
2024-10-22 09:03:32 +0700

    feat: add data fetching and display for `Beranda` and `Prestasi` pages

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -2,21 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BidangLomba;
+use App\Models\Juara;
+use App\Models\Prestasi;
 use Illuminate\Http\Request;
 
 class RootController extends Controller
 {
-    public function Beranda()
+    public function Beranda(Request $request)
     {
-        return view("beranda");
+        $prestasi = Prestasi::join("juara", "prestasi.juara_id", "juara.id")
+            ->join("bidang_lomba", "prestasi.bidang_lomba_id", "bidang_lomba.id")
+            ->select("prestasi.*", "juara.nama as juara", "bidang_lomba.nama as bidang_lomba")
+            ->get();
+
+        $jumlahPrestasi = Prestasi::count();
+        $jumlahJuara = Juara::count();
+        $jumlahBidangLomba = BidangLomba::count();
+
+        return view("beranda.beranda", compact("prestasi", "jumlahPrestasi", "jumlahJuara", "jumlahBidangLomba"));
     }
 
-    public function Prestasi()
+    public function Prestasi(Request $request)
     {
-        return view("prestasi");
+        $prestasi = Prestasi::join("juara", "prestasi.juara_id", "juara.id")
+            ->join("bidang_lomba", "prestasi.bidang_lomba_id", "bidang_lomba.id")
+            ->select("prestasi.*", "juara.nama as juara", "bidang_lomba.nama as bidang_lomba")
+            ->get();
+
+        $juara = Juara::all();
+        $bidangLomba = BidangLomba::all();
+
+        return view("data-prestasi.data-prestasi", compact("prestasi", "juara", "bidangLomba"));
     }
 
-    public function Login()
+    public function Tentang(Request $request)
+    {
+        return view("tentang.tentang", compact("prestasi"));
+    }
+
+    public function Login(Request $request)
     {
         return view("login");
     }
