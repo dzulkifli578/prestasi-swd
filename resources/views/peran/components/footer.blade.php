77cbdba83f05965268f66218b6e14bbd9565f4bd
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:52:07 +0700

    feat: add footer for role-based components

---
+++ resources/views/peran/components/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="footer bg-primary text-primary-content items-center p-6">
+    <aside class="grid-flow-col items-center">
+        <img src="{{ asset('img/swadhipa.avif') }}" alt="Logo Swadhipa" class="w-12 h-12">
+      <p class="text-sm md:text-base font-medium">Copyright © {{ date('Y') }} - All right reserved</p>
+    </aside>
+    <nav class="grid-flow-col gap-6 md:place-self-center md:justify-self-end">
+      <a href="https://www.youtube.com/@smkswadhipa2natar">
+        <i class="fa-brands fa-youtube text-xl"></i>
+      </a>
+      <a href="https://www.tiktok.com/@smkswadhipa2natar">
+        <i class="fa-brands fa-tiktok text-xl"></i>
+      </a>
+      <a href="https://www.instagram.com/smkswadhipa2/">
+        <i class="fa-brands fa-instagram text-xl"></i>
+      </a>
+      <a href="https://www.facebook.com/smkswadhipa2/?locale=id_ID">
+        <i class="fa-brands fa-facebook text-xl"></i>
+      </a>
+    </nav>
+  </footer>
