9e87ad4b95c79e580be3f673100bd0a70eee679e
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:55:52 +0700

    refactor: simplify about page layout and add new sections

--- resources/views/tentang/tentang.blade.php
+++ resources/views/tentang/tentang.blade.php
@@ -8,23 +8,17 @@
     <title>Tentang</title>
     <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
-    <script defer src="{{ asset('js/alpine.js') }}"></script>
     <script defer src="{{ asset('js/turbo.js') }}"></script>
 </head>
 
-<body>
+<body class="body-section">
     @include('components.navbar')
 
     <turbo-frame id="main-content">
-        <div class="bg-base-300 rounded-xl shadow-xl p-6 m-6">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl text-center font-bold mb-6">Tentang Web Ini</h1>
-            @include('tentang.penjelasan')
-        </div>
-
-        <div class="bg-base-300 rounded-xl shadow-xl p-6 m-6">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl text-center font-bold mb-6">Kontributor</h1>
-            @include('tentang.kontributor')
-        </div>
+        @include('tentang.penjelasan')
+        @include('tentang.kontributor')
+        @include('tentang.teknologi')
+        @include('tentang.detail')
     </turbo-frame>
 
     @include('components.footer')
