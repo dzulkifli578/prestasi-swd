ce744b24afd032286c3df2c12bb103c631f5de83
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:37:52 +0700

    refactor: update search and filter layout

--- resources/views/data-prestasi/cari-filter.blade.php
+++ resources/views/data-prestasi/cari-filter.blade.php
@@ -1,25 +1,26 @@
-<div class="bg-base-100 rounded-xl shadow-xl p-6">
-    <div class="flex flex-col gap-y-6">
-        <input type="text" placeholder="Cari berdasarkan nama." class="input input-bordered input-primary w-full"
-            id="searchInput" />
-        <div class="grid grid-cols-1 gap-y-4 lg:grid-cols-3 lg:gap-x-6">
-            <select class="select select-bordered select-primary w-full" id="juaraFilter">
-                <option value="" selected>Semua Juara</option>
-                @foreach ($juara as $itemJuara)
-                    <option value="{{ strtolower($itemJuara->nama) }}">{{ $itemJuara->nama }}</option>
-                @endforeach
-            </select>
-            <select class="select select-bordered select-primary w-full" id="lombaFilter">
-                <option value="" selected>Semua Lomba</option>
-                @foreach ($bidangLomba as $itemBidangLomba)
-                    <option value="{{ strtolower($itemBidangLomba->nama) }}">{{ $itemBidangLomba->nama }}</option>
-                @endforeach
-            </select>
-            <select class="select select-bordered select-primary w-full" id="waktuFilter">
-                <option value="" selected>Urutkan</option>
-                <option value="terbaru">Terbaru</option>
-                <option value="terlama">Terlama</option>
-            </select>
+<div class="card-section-primary transition-smooth">
+    <h1 class="card-title-primary hover-scale-sm">Cari & Filter</h1>
+
+    <div class="card-section-secondary">
+        <div class="flex flex-col gap-y-6">
+            <label class="input input-bordered input-primary flex items-center gap-2">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text" class="grow" placeholder="Cari" id="searchInput" />
+            </label>
+            <div class="grid grid-cols-1 gap-y-4 md:grid-cols-2 md:gap-x-6">
+                <select class="select select-bordered select-primary w-full" id="juaraFilter">
+                    <option value="" selected>Semua Juara</option>
+                    @foreach ($juara as $itemJuara)
+                        <option value="{{ strtolower($itemJuara->nama) }}">{{ $itemJuara->nama }}</option>
+                    @endforeach
+                </select>
+                <select class="select select-bordered select-primary w-full" id="lombaFilter">
+                    <option value="" selected>Semua Lomba</option>
+                    @foreach ($bidangLomba as $itemBidangLomba)
+                        <option value="{{ strtolower($itemBidangLomba->nama) }}">{{ $itemBidangLomba->nama }}</option>
+                    @endforeach
+                </select>
+            </div>
         </div>
     </div>
 </div>
