9be2a2a7ea811cdfc583d0fcd0fd8af80ee89549
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 09:20:15 +0700

    refactor: update session usage in `AdminMiddleware`

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Session;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -25,7 +24,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!Session::has(["id", "nama_pengguna"]))
+        if (session()->has(["id", "nama_pengguna"]) || session()->get("peran") != "admin")
             return redirect()->route("login")->with("error", 'Anda tidak memiliki akses sebagai admin.');
 
         return $next($request);
