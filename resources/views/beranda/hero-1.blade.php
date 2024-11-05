7998866930b3af2fb36797ac0c4a3a71b5c5530c
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:33:28 +0700

    style: update hero section with hover effects

--- resources/views/beranda/hero-1.blade.php
+++ resources/views/beranda/hero-1.blade.php
@@ -1,11 +1,11 @@
 <div class="hero min-h-screen"
-    style="background-image: url({{ asset('img/hero1.jpg') }});">
-    <div class="hero-overlay bg-opacity-60"></div>
+    style="background-image: url({{ asset('img/hero1.avif') }});">
+    <div class="hero-overlay bg-opacity-50 hover:bg-opacity-20 transition-colors transition-smooth"></div>
     <div class="hero-content text-neutral-content text-center">
-        <div class="max-w-md">
-            <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold">Web Prestasi Siswa</h1>
-            <h2 class="my-6 text-xl md:text-2xl lg:text-3xl font-semibold">SMK Swadhipa 2 Natar</h2>
-            <button class="btn btn-primary" onclick="window.location.href='{{ route('tentang') }}'">Tentang Web Ini</button>
+        <div class="max-w-lg">
+            <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold tracking-wide hover-scale-md">Web Prestasi Siswa</h1>
+            <h2 class="text-xl md:text-2xl lg:text-3xl font-semibold tracking-wide my-6 hover-scale-md">SMK Swadhipa 2 Natar</h2>
+            <button class="btn btn-primary hover-scale-md" onclick="window.location.href='{{ route('tentang') }}'">Tentang Web Ini</button>
         </div>
     </div>
 </div>
--- resources/views/beranda/hero-2.blade.php
+++ resources/views/beranda/hero-2.blade.php
@@ -1,11 +1,11 @@
-<div class="hero bg-base-200 min-h-screen">
+<div class="group hero bg-base-200 min-h-screen hover:bg-warning ease-in-out duration-300">
     <div class="hero-content text-center">
         <div class="max-w-md">
-            <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold">Tertarik?</h1>
-            <p class="py-6">
-                Silahkan kunjungi halaman data prestasi siswa untuk melihat lebih banyaknya
+            <h1 class="tracking-wider text-3xl md:text-4xl lg:text-5xl font-bold hover-scale-md group-hover:text-warning-content">Tertarik?</h1>
+            <p class="text-lg font-medium tracking-wider hover-scale-md py-6 group-hover:text-warning-content">
+                Silahkan kunjungi halaman data prestasi siswa untuk melihat lebih banyak.
             </p>
-            <button onclick="window.location.href='{{ route('prestasi') }}'" class="btn btn-primary">Prestasi
+            <button onclick="window.location.href='{{ route('prestasi') }}'" class="btn btn-primary hover:scale-110 group-hover:btn-neutral group-hover:text-neutral-200 font-semibold ease-in-out duration-300">Prestasi
                 Siswa</button>
         </div>
     </div>
