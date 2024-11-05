f6929e88e61bf2c0b42be9f91e571ab497664feb
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:42:29 +0700

    refactor: simplify layout and remove unnecessary elements

--- resources/views/data-prestasi/data-prestasi.blade.php
+++ resources/views/data-prestasi/data-prestasi.blade.php
@@ -5,26 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prestasi</title>
+    <title>Data Prestasi</title>
     <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
-    <script src="{{ asset('js/alpine.js') }}" defer></script>
+    <script defer src="{{ asset('js/turbo.js') }}"></script>
 </head>
 
-<body>
+<body class="body-section">
     @include('components.navbar')
 
     <turbo-frame id="main-content">
-        <div class="bg-base-300 rounded-xl shadow-xl p-6 m-6">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl text-center font-bold mb-6">Cari & Filter</h1>
-            @include('data-prestasi.cari-filter')
-        </div>
-
-        <div class="bg-base-300 rounded-xl shadow-xl p-6 m-6">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl text-center font-bold mb-6">Data Prestasi Siswa</h1>
-            @include('data-prestasi.grid')
-            @include('data-prestasi.detail')
-        </div>
+        @include('data-prestasi.cari-filter')
+        @include('data-prestasi.grid')
+        @include('data-prestasi.detail')
     </turbo-frame>
 
     @include('components.footer')
