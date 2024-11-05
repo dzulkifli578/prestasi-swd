832b45ee7cc875322d65b40f234f597943144f6e
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:35:48 +0700

    feat: refactor statistics section with card design

--- resources/views/beranda/statistik.blade.php
+++ resources/views/beranda/statistik.blade.php
@@ -1,30 +1,40 @@
-<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-    <div class="group bg-base-100 hover:bg-warning ease-in-out duration-500 rounded-xl shadow-xl p-6">
-        <div class="flex flex-row items-center gap-x-6">
-            <span class="fa-solid fa-trophy text-xl group-hover:text-warning-content ease-in-out duration-500"></span>
-            <div class="flex flex-col gap-y-1 group-hover:text-warning-content ease-in-out duration-500">
-                <p class="text-lg md:text-xl lg:text-2xl font-bold">Jumlah Prestasi</p>
-                <p class="text-base md:text-lg lg:text-xl font-medium">{{ $jumlahPrestasi }}</p>
+<div class="card-section-primary transition-smooth">
+    <h1 class="card-title-primary hover-scale-sm">Statistik</h1>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div class="group card-section-secondary transition-smooth hover:bg-warning">
+            <div class="flex flex-row justify-between items-center">
+                <div class="flex flex-col gap-y-6 group-hover:text-warning-content transition-smooth">
+                    <p class="text-sm md:text-base lg:text-md font-semibold tracking-wide">Jumlah Prestasi</p>
+                    <p class="text-2xl md:text-3xl lg:text-4xl font-bold tracking-wide">{{ $jumlahPrestasi }}</p>
+                </div>
+                <div class="bg-base-300 rounded-xl p-6 group-hover:bg-base-300 hover:rounded-full transition-slow">
+                    <i class="fa-solid fa-trophy text-3xl group-hover:text-neutral-200 transition-smooth"></i>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="group bg-base-100 hover:bg-warning ease-in-out duration-500 rounded-xl shadow-xl p-6">
-        <div class="flex flex-row items-center gap-x-6">
-            <span class="fa-solid fa-trophy text-xl group-hover:text-warning-content ease-in-out duration-500"></span>
-            <div class="flex flex-col gap-y-1 group-hover:text-warning-content ease-in-out duration-500">
-                <p class="text-lg md:text-xl lg:text-2xl font-bold">Jumlah Juara</p>
-                <p class="text-base md:text-lg lg:text-xl font-medium">{{ $jumlahJuara }}</p>
+        <div class="group card-section-secondary transition-smooth hover:bg-warning">
+            <div class="flex flex-row justify-between items-center">
+                <div class="flex flex-col gap-y-6 group-hover:text-warning-content transition-smooth">
+                    <p class="text-sm md:text-base lg:text-md font-semibold tracking-wide">Jumlah Juara</p>
+                    <p class="text-2xl md:text-3xl lg:text-4xl font-bold">{{ $jumlahJuara }}</p>
+                </div>
+                <div class="bg-base-300 rounded-xl p-6 group-hover:bg-base-300 hover:rounded-full transition-slow">
+                    <i class="fa-solid fa-medal text-3xl group-hover:text-neutral-200 transition-smooth"></i>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="group bg-base-100 hover:bg-warning ease-in-out duration-500 rounded-xl shadow-xl p-6">
-        <div class="flex flex-row items-center gap-x-6">
-            <span class="fa-solid fa-trophy text-xl group-hover:text-warning-content ease-in-out duration-500"></span>
-            <div class="flex flex-col gap-y-1 group-hover:text-warning-content ease-in-out duration-500">
-                <p class="text-lg md:text-xl lg:text-2xl font-bold">Jumlah Bidang Lomba</p>
-                <p class="text-base md:text-lg lg:text-xl font-medium">{{ $jumlahBidangLomba }}</p>
+        
+        <div class="group card-section-secondary transition-smooth hover:bg-warning">
+            <div class="flex flex-row justify-between items-center">
+                <div class="flex flex-col gap-y-6 group-hover:text-warning-content transition-smooth">
+                    <p class="text-sm md:text-base lg:text-md font-semibold tracking-wide">Jumlah Bidang Lomba</p>
+                    <p class="text-2xl md:text-3xl lg:text-4xl font-bold">{{ $jumlahBidangLomba }}</p>
+                </div>
+                <div class="bg-base-300 rounded-xl p-6 group-hover:bg-base-300 hover:rounded-full transition-slow">
+                    <i class="fa-solid fa-crosshairs text-3xl group-hover:text-neutral-200 transition-smooth"></i>
+                </div>
             </div>
         </div>
     </div>
