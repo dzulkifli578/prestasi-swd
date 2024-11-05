66b1b3bcd3258143581a1c698ebe14cbd39a384e
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:56:48 +0700

    feat: improve `penjelasan` section styling and add card section

--- resources/views/tentang/penjelasan.blade.php
+++ resources/views/tentang/penjelasan.blade.php
@@ -1,6 +1,12 @@
-<p class="text-lg md:text-xl leading-relaxed">
-    Web Prestasi Siswa SMK Swadhipa 2 Natar dibuat untuk mencatat dan menampilkan prestasi siswa dalam berbagai bidang akademik dan non-akademik. Tujuannya adalah memudahkan publik untuk mengetahui pencapaian siswa, serta memberikan motivasi kepada siswa lain untuk terus berprestasi. Melalui web ini, informasi prestasi dapat diakses secara transparan dan cepat.
-</p>
-<p class="text-lg md:text-xl leading-relaxed mt-4">
-    Dengan fitur pencarian dan filter prestasi, pengguna dapat dengan mudah menemukan informasi yang relevan sesuai kategori, waktu, dan jenis lomba. Website ini dikembangkan menggunakan teknologi modern dengan tujuan memberikan pengalaman pengguna yang responsif dan interaktif.
-</p>
+<div class="card-section-primary transition-smooth">
+    <h1 class="card-title-primary hover-scale-sm">Tentang Web Ini</h1>
+    <div class="border border-primary my-6"></div>
+
+    <p class="text-lg md:text-xl leading-relaxed">
+        Web Prestasi Siswa SMK Swadhipa 2 Natar dibuat untuk mencatat dan menampilkan prestasi siswa dalam berbagai bidang akademik dan non-akademik. Tujuannya adalah memudahkan publik untuk mengetahui pencapaian siswa, serta memberikan motivasi kepada siswa lain untuk terus berprestasi. Melalui web ini, informasi prestasi dapat diakses secara transparan dan cepat.
+    </p>
+    <p class="text-lg md:text-xl leading-relaxed mt-4">
+        Dengan fitur pencarian dan filter prestasi, pengguna dapat dengan mudah menemukan informasi yang relevan sesuai kategori, waktu, dan jenis lomba. Website ini dikembangkan menggunakan teknologi modern dengan tujuan memberikan pengalaman pengguna yang responsif dan interaktif.
+    </p>
+</div>
+
