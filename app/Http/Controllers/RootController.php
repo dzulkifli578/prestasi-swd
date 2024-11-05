827bb619e02c279fee488ea7312123a28825646c
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 08:36:48 +0700

    refactor: improve `RootController` with Eloquent relationships and docblocks

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -7,27 +7,47 @@ use App\Models\Juara;
 use App\Models\Prestasi;
 use Illuminate\Http\Request;
 
+/**
+ * Class DashboardController
+ *
+ * Controller for handling root features.
+ *
+ * @package App\Http\Controllers\Admin
+ */
 class RootController extends Controller
 {
+    /**
+     * Display the root dashboard.
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\View
+     */
     public function Beranda(Request $request)
     {
-        $prestasi = Prestasi::join("juara", "prestasi.juara_id", "juara.id")
-            ->join("bidang_lomba", "prestasi.bidang_lomba_id", "bidang_lomba.id")
-            ->select("prestasi.*", "juara.nama as juara", "bidang_lomba.nama as bidang_lomba")
-            ->get();
+        $prestasi = Prestasi::with("juara", "bidangLomba")
+            ->select("prestasi.*", "tahun")
+            ->orderByDesc("id")
+            ->paginate(6);
 
         $jumlahPrestasi = Prestasi::count();
         $jumlahJuara = Juara::count();
         $jumlahBidangLomba = BidangLomba::count();
+        $juara = Juara::all();
+        $bidangLomba = BidangLomba::all();
 
-        return view("beranda.beranda", compact("prestasi", "jumlahPrestasi", "jumlahJuara", "jumlahBidangLomba"));
+        return view("beranda.beranda", compact("prestasi", "jumlahPrestasi", "jumlahJuara", "jumlahBidangLomba", "juara", "bidangLomba"));
     }
 
+    /**
+     * Display the 'prestasi' data
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function Prestasi(Request $request)
     {
-        $prestasi = Prestasi::join("juara", "prestasi.juara_id", "juara.id")
-            ->join("bidang_lomba", "prestasi.bidang_lomba_id", "bidang_lomba.id")
-            ->select("prestasi.*", "juara.nama as juara", "bidang_lomba.nama as bidang_lomba")
+        $prestasi = Prestasi::with("juara", "bidangLomba")
+            ->select("prestasi.*", "tahun")
+            ->orderByDesc("id")
             ->get();
 
         $juara = Juara::all();
@@ -36,11 +56,21 @@ class RootController extends Controller
         return view("data-prestasi.data-prestasi", compact("prestasi", "juara", "bidangLomba"));
     }
 
+    /**
+     * Display 'tentang' page
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function Tentang(Request $request)
     {
-        return view("tentang.tentang", compact("prestasi"));
+        return view("tentang.tentang");
     }
 
+    /**
+     * Display 'login' page
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function Login(Request $request)
     {
         return view("login");
