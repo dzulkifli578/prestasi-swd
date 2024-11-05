07c52441a11a90b63f1363fbee552be5e1696b26
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 08:28:55 +0700

    feat: add `Inter` typography and animation to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,27 @@ export default {
     "./resources/views/**/*.blade.php",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Inter", "sans-serif"]
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(-3deg)' },
+          '75%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        shake: 'shake 0.5s ease-in-out',
+      },
+    },
   },
   plugins: [
     require('daisyui')
   ],
   daisyui: {
-    themes: ["dark"],
+    themes: ["dark"]
   },
 }
 
