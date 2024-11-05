1cbbcc53c1c8a50e82e675b6528cbfe7d852477c
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:37:05 +0700

    refactor: update navbar and drawer layout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,28 +16,28 @@
 @php
     $menu = [
         ['route' => 'beranda', 'label' => 'Beranda', 'icon' => 'fa-solid fa-house'],
-        ['route' => 'prestasi', 'label' => 'Prestasi', 'icon' => 'fa-solid fa-list'],
+        ['route' => 'prestasi', 'label' => 'Data Prestasi', 'icon' => 'fa-solid fa-list'],
         ['route' => 'tentang', 'label' => 'Tentang', 'icon' => 'fa-solid fa-info'],
         ['route' => 'login', 'label' => 'Login', 'icon' => 'fa-solid fa-right-to-bracket'],
     ];
 @endphp
 
-<div class="navbar bg-base-100 border-b border-neutral-content">
+<div class="fixed top-0 navbar bg-base-100 border-b border-neutral-content z-10">
     <div class="navbar-start">
-        <div class="drawer-content">
-            <label for="my-drawer" class="flex flex-row items-center cursor-pointer gap-x-4">
-                <img src="{{ asset('img/swadhipa.png') }}" alt="Logo Swadhipa" class="h-12">
-                <a class="text-xl xl:text-2xl text-nowrap font-semibold">SMK Swadhipa 2 Natar</a>
-            </label>
-        </div>
+        <a class="navbar-text ml-6 truncate cursor-default">SMK Swadhipa 2 Natar</a>
+    </div>
+    <div class="navbar-end">
+        <label for="my-drawer" class="cursor-pointer mr-6">
+            <img src="{{ asset('img/swadhipa.avif') }}" alt="Logo Swadhipa" class="h-12 hover-scale-sm">
+        </label>
     </div>
 </div>
 
-<div class="drawer z-10">
+<div class="drawer drawer-end z-20">
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-side">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu bg-base-300 text-base-content min-h-full w-52 p-6">
+        <ul class="menu bg-base-300 text-base-content min-h-full w-60 p-6">
             <div class="w-full flex flex-col justify-center">
                 <p class="text-lg md:text-xl lg:text-2xl font-semibold">{{ $greeting }}</p>
                 <div class="border border-primary my-4"></div>
@@ -45,8 +45,8 @@
             <div class="text-lg">
                 @foreach ($menu as $menu)
                     <li><a href="{{ route($menu['route']) }}"
-                            class="{{ Route::is($menu['route']) ? 'text-primary' : '' }}"><i
-                                class="{{ $menu['icon'] }}"></i> {{ $menu['label'] }}</a>
+                            class="{{ Route::is($menu['route']) ? 'text-primary' : '' }}">
+                            <i class="{{ $menu['icon'] }} inline-flex text-center min-w-6 mr-2"></i> {{ $menu['label'] }}</a>
                     </li>
                 @endforeach
             </div>
