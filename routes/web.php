ed9dd50cd877ed095cdbb708465bc3e5fda6c0ed
da578 <dzulkiflianwar2@gmail.com>
2024-11-05 10:59:54 +0700

    refactor: split routes into separate files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Peran\Admin\AdminController;
+use App\Http\Controllers\Peran\AdminManager\AdminManagerController;
 use App\Http\Controllers\RootController;
+use App\Http\Middleware\AdminManagerMiddleware;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\AuthMiddleware;
 use App\Http\Middleware\GuestMiddleware;
@@ -9,10 +11,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix("/")->group(function () {
     Route::get("", [RootController::class, "Beranda"])->name("beranda");
-    Route::get("prestasi", [RootController::class, "Prestasi"])->name("prestasi");
+    Route::get("data-prestasi", [RootController::class, "Prestasi"])->name("prestasi");
     Route::get("tentang", [RootController::class, "Tentang"])->name("tentang");
-    Route::get("login", [RootController::class, "Login"])->name("login");
-    Route::post("proses-login")->middleware(AuthMiddleware::class)->name("proses-login");
 });
 
 Route::prefix("/login")->middleware(GuestMiddleware::class)->group(function () {
@@ -21,9 +21,9 @@ Route::prefix("/login")->middleware(GuestMiddleware::class)->group(function () {
 });
 
 Route::prefix("/admin")->middleware(AdminMiddleware::class)->group(function () {
-    Route::get("/dashboard", [AdminController::class, "Dashboard"])->name("dashboard");
-    Route::get("/profil", [AdminController::class, "Profil"])->name("profil");
-    Route::put("/profil/perbarui", [AdminController::class, "PerbaruiProfil"])->name("perbarui-profil");
+    Route::get("/dashboard", [AdminController::class, "Dashboard"])->name("dashboard-admin");
+    Route::get("/profil", [AdminController::class, "Profil"])->name("profil-admin");
+    Route::put("/profil/perbarui", [AdminController::class, "PerbaruiProfil"])->name("perbarui-profil-admin");
 
     Route::prefix("/data-prestasi")->group(function () {
         Route::get("/", [AdminController::class, "DataPrestasi"])->name("data-prestasi");
@@ -43,7 +43,28 @@ Route::prefix("/admin")->middleware(AdminMiddleware::class)->group(function () {
         Route::delete("/hapus-bidang-lomba/{id}", [AdminController::class, "HapusBidangLomba"])->name("hapus-bidang-lomba");
     });
 
-    Route::get("/log", [AdminController::class, "Log"])->name("log");
+    Route::prefix("/log")->group(function () {
+        Route::get("", [AdminController::class, "Log"])->name("log-admin");
+        Route::delete("/hapus/{id}", [AdminController::class, "HapusLog"])->name("hapus-log-admin");
+    });
 
-    Route::post("/logout", [AdminController::class, "Logout"])->name("logout");
+    Route::post("/logout", [AdminController::class, "Logout"])->name("logout-admin");
+});
+
+Route::prefix("/admin-manager")->middleware(AdminManagerMiddleware::class)->group(function () {
+    Route::get("/dashboard", [AdminManagerController::class, "Dashboard"])->name("dashboard-admin-manager");
+    Route::get("/profil", [AdminManagerController::class, "Profil"])->name("profil-admin-manager");
+    Route::put("/profil/perbarui", [AdminManagerController::class, "PerbaruiProfil"])->name("perbarui-profil-admin-manager");
+
+    Route::get("/data-admin", [AdminManagerController::class, "DataAdmin"])->name("data-admin");
+    Route::post("/data-admin/tambah", [AdminManagerController::class, "TambahDataAdmin"])->name("tambah-data-admin");
+    Route::put("/data-admin/edit/{id}", [AdminManagerController::class, "EditDataAdmin"])->name("edit-data-admin");
+    Route::delete("/data-admin/hapus/{id}", [AdminManagerController::class, "HapusDataAdmin"])->name("hapus-data-admin");
+
+    Route::prefix("/log")->group(function () {
+        Route::get("", [AdminManagerController::class, "Log"])->name("log-admin-manager");
+        Route::delete("/hapus/{id}", [AdminManagerController::class, "HapusLog"])->name("hapus-log-admin-manager");
+    });
+
+    Route::post("/logout", [AdminManagerController::class, "Logout"])->name("logout-admin-manager");
 });
